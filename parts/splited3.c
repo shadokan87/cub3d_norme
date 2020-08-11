e1268086046ea8d5fea5a91b787a36a3abfea57e
shadokan87 <shinobi87@live.fr>
2020-08-12 00:11:10 +0200

    ds@idisgh2151ni

--- parts/splited3.c
+++ parts/splited3.c
@@ -10,7 +10,7 @@ static void	initspritequeue2(t_var *var)
 		var->spritequeue[i] = malloc(sizeof(int) * 2);
 }
 
-void	initspritequeue(t_var *var)
+void		initspritequeue(t_var *var)
 {
 	int i;
 	int y;
@@ -21,22 +21,21 @@ void	initspritequeue(t_var *var)
 	z = 0;
 	initspritequeue2(var);
 	while (++y < var->m_height)
-  	{
-    while (++i < var->m_width)
-  	{
-		if (var->map[y][i] == 6)
+	{
+		while (++i < var->m_width)
 		{
-			var->map[y][i] = 0;
-			var->s_spritex = y;
-			var->s_spritey = i;
-			var->spritequeue[z][0] = y;
-			var->spritequeue[z][1] = i;
-			z++;
+			if (var->map[y][i] == 6)
+			{
+				var->map[y][i] = 0;
+				var->s_spritex = y;
+				var->s_spritey = i;
+				var->spritequeue[z][0] = y;
+				var->spritequeue[z][1] = i;
+				z++;
+			}
 		}
-  	}
-  i = -1;
-  }
-
+		i = -1;
+	}
 }
 
 static void	duplicate_map2(t_var *var)
@@ -47,44 +46,44 @@ static void	duplicate_map2(t_var *var)
 	var->map = malloc(sizeof(int *) * var->m_height);
 	var->spritenum = 0;
 	while (++i < var->m_height)
-    	var->map[i] = malloc(sizeof(int) * var->m_width);
+		var->map[i] = malloc(sizeof(int) * var->m_width);
 }
 
-void duplicate_map(t_var *var, char **str2)
+void		duplicate_map(t_var *var, char **str2)
 {
-	char **str;
-	char c;
-	int i;
-	int y;
-	
+	char	**str;
+	char	c;
+	int		i;
+	int		y;
+
 	i = 0;
 	y = -1;
 	duplicate_map2(var);
-  while (++y < var->m_height)
-  {
-    while (++i < var->m_width)
-  	{
-		 str = ft_split(str2[y], ' ');
-		 var->map[y][i] = str[i][0] - '0';
-		 if (var->map[y][i] == 6)
-			var->spritenum++;
-		if (var->map[y][i] == 4)
+	while (++y < var->m_height)
+	{
+		while (++i < var->m_width)
 		{
-			var->posx = y + 1.5;
-			var->posy = i + 1;
+			str = ft_split(str2[y], ' ');
+			var->map[y][i] = str[i][0] - '0';
+			if (var->map[y][i] == 6)
+				var->spritenum++;
+			if (var->map[y][i] == 4)
+			{
+				var->posx = y + 1.5;
+				var->posy = i + 1;
+			}
 		}
-  	}
-  i = -1;
-  }
+		i = -1;
+	}
 }
 
-char	**getmapstr(t_var *var)
+char		**getmapstr(t_var *var)
 {
-	int width;
-	char *str;
-	char **split;
-	char *ptr;
-	int z;
+	int		width;
+	char	*str;
+	char	**split;
+	char	*ptr;
+	int		z;
 
 	str = ft_strdup(var->paramfile);
 	split = ft_split(str, '\n');
@@ -95,6 +94,5 @@ char	**getmapstr(t_var *var)
 		free(*split);
 		split++;
 	}
-		
 	return (split);
 }
