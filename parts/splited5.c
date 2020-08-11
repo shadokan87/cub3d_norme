555a281492e816110a0ac5cc95d271dd6fdef714
shadokan87 <shinobi87@live.fr>
2020-08-12 00:37:45 +0200

    splited5

--- parts/splited5.c
+++ parts/splited5.c
@@ -1,27 +1,27 @@
 #include "../cublib.h"
 
-void drawbmp (char *filename, t_var *var)
+void	drawbmp(char *filename, t_var *var)
 {
-unsigned int *headers;
-int extrabytes;
-int paddedsize;
-int fd;
+	unsigned int	*headers;
+	int				extrabytes;
+	int				paddedsize;
+	int				fd;
 
-fd = open(filename, O_TRUNC | O_RDWR | O_APPEND | O_CREAT);
-extrabytes = 4 - ((var->s_w * 3) % 4) % 4;                 
-if (extrabytes == 4)
-   extrabytes = 0;
-paddedsize = ((var->s_w * 3) + extrabytes) * var->s_h;
-headers = getheader(var->s_w, var->s_h, paddedsize);                  
-ft_fprintf(fd, "BM");
-writeheader(fd, headers);
-writecolor(var, extrabytes, fd);
-close(fd);
-closegame(var, "--save done");
-return;
+	fd = open(filename, O_TRUNC | O_RDWR | O_APPEND | O_CREAT);
+	extrabytes = 4 - ((var->s_w * 3) % 4) % 4;
+	if (extrabytes == 4)
+		extrabytes = 0;
+	paddedsize = ((var->s_w * 3) + extrabytes) * var->s_h;
+	headers = getheader(var->s_w, var->s_h, paddedsize);
+	ft_fprintf(fd, "BM");
+	writeheader(fd, headers);
+	writecolor(var, extrabytes, fd);
+	close(fd);
+	closegame(var, "--save done");
+	return ;
 }
 
-int	screenshot(t_var *var)
+int		screenshot(t_var *var)
 {
 	int i;
 
@@ -30,10 +30,11 @@ int	screenshot(t_var *var)
 	while (i < var->spritenum)
 	{
 		var->dist = getdist(var);
-		draw_sprite(var, var->spritequeue[var->spriteorder[i]][0], var->spritequeue[var->spriteorder[i]][1]);
+		draw_sprite(var, var->spritequeue[var->spriteorder[i]][0],
+		var->spritequeue[var->spriteorder[i]][1]);
 		i++;
-	}	
-	drawbmp("screenshot2.bmp",var);
+	}
+	drawbmp("screenshot.bmp", var);
 	return (1);
 }
 
@@ -42,11 +43,13 @@ int		run(t_var *var)
 	int t_endian;
 	int t_bpp;
 	int t_line;
-	int i = 0;
+	int i;
 
+	i = -1;
 	var->tex_h = 64;
 	var->tex_w = 64;
-	var->img_data = (int *)mlx_get_data_addr(var->img, &var->t_bpp, &var->t_line, &var->t_endian);
+	var->img_data = (int *)mlx_get_data_addr(var->img,
+	&var->t_bpp, &var->t_line, &var->t_endian);
 	if (var->screenshot)
 		screenshot(var);
 	listen_keys(var);
@@ -54,12 +57,12 @@ int		run(t_var *var)
 	|| var->r_r || var->l_r)
 		cls(var);
 	raycast(var);
-	while (i < var->spritenum)
+	while (++i < var->spritenum)
 	{
 		var->dist = getdist(var);
-		draw_sprite(var, var->spritequeue[var->spriteorder[i]][0], var->spritequeue[var->spriteorder[i]][1]);
-		i++;
-	}	
+		draw_sprite(var, var->spritequeue[var->spriteorder[i]][0],
+		var->spritequeue[var->spriteorder[i]][1]);
+	}
 	mlx_put_image_to_window(var->mlx_ptr, var->mlx_win, var->img, 0, 0);
 }
 
@@ -70,33 +73,30 @@ int		start(t_var *var)
 	mlx_loop(var->mlx_ptr);
 }
 
-int	**copymap(int height, int width, int index, t_var *var)
+int		**copymap(int height, int width, int index, t_var *var)
 {
-	int **map;
-	char **str;
-	char *tmp;
-	int i;
-	int y;
+	int		**map;
+	char	**str;
+	char	*tmp;
+	int		i;
+	int		y;
 
-	i = 0;
+	i = -1;
 	y = 0;
 	map = malloc(sizeof(int *) * height);
 	str = ft_split(&var->paramfile[index], '\n');
-	while (i < height)
-  	{
-    	map[i] = malloc(sizeof(int) * width);
-    	i++;
-  	}
+	while (++i < height)
+		map[i] = malloc(sizeof(int) * width);
 	i = 0;
-		while (str[i])
+	while (str[i])
+	{
+		while (ft_split(str[i], ' ')[y])
 		{
-			while (ft_split(str[i], ' ')[y])
-			{
-				map[i][y] = (ft_split(str[i], ' '))[y][0] - 0;
-				y++;
-			}
-			y = 0;
-			i++;			
+			map[i][y] = (ft_split(str[i], ' '))[y][0] - 0;
+			y++;
 		}
+		y = 0;
+		i++;
+	}
 	return (map);
 }
